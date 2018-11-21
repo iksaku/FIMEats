fe624980fae9e701971b64a763bc16c625eed100
Jorge González <iksaku@me.com>
2018-11-21 11:37:37 -0600

    Last time changes

--- resources/views/template/components/head.blade.php
+++ resources/views/template/components/head.blade.php
@@ -8,6 +8,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 
+    <link rel="icon" href="{{ asset('favicon.png') }}">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}" />
     @stack('stylesheets')
 </head>
