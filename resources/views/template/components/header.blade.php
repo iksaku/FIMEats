d878a57e63dcb4fba276824163c05287130dc323
Jorge González <iksaku@me.com>
2019-03-11 15:46:09 -0600

    Added Github link

--- resources/views/template/components/header.blade.php
+++ resources/views/template/components/header.blade.php
@@ -4,6 +4,12 @@
         <ul>
             <li><a href="{{ route('index') }}">Inicio</a></li>
             <li><a href="{{ route('index') . '#facultades' }}">Selecciona tu Facultad</a></li>
+            <li>
+                <a href="https://github.com/iksaku/FIMEats">
+                    Contribuye en Github
+                    <span class="fa fa-github" style="font-size: 30px; margin: 6px 0 0 6px;"></span>
+                </a>
+            </li>
         </ul>
     </nav>
 </header>
