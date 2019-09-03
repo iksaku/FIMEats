46e2ae2e6957112bc20b4130d7f80b1dabf3b63d
Jorge González <iksaku@me.com>
2019-09-03 18:44:01 -0500

    Fixed shadow-outline overlapping borders

--- resources/js/components/FacultyCard.vue
+++ resources/js/components/FacultyCard.vue
@@ -1,17 +1,19 @@
 <template>
-    <inertia-link
-        :href="route('faculty.show', short_name.toLowerCase())"
-        class="faculty-card w-full sm:w-1/2 py-6 border-t-2 border-gray-300 text-center hover:shadow-outline focus:outline-none focus:shadow-outline"
-    >
-        <img
-            class="h-48 w-48 rounded-lg mx-auto"
-            :src="logo"
-            :alt="`Logo of ${short_name} Faculty`"
+    <div class="faculty-card w-full sm:w-1/2 p-6">
+        <inertia-link
+            :href="route('faculty.show', short_name.toLowerCase())"
+            class="w-full block text-center hover:shadow-outline focus:outline-none focus:shadow-outline"
         >
-        <h3 class="text-center text-gray-700 text-xl uppercase mt-2">
-            {{ short_name }}
-        </h3>
-    </inertia-link>
+            <img
+                class="h-48 w-48 rounded-lg mx-auto"
+                :src="logo"
+                :alt="`Logo of ${short_name} Faculty`"
+            >
+            <h3 class="text-center text-gray-700 text-xl uppercase mt-2">
+                {{ short_name }}
+            </h3>
+        </inertia-link>
+    </div>
 </template>
 
 <script>
