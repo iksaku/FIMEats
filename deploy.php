31d1d791ebe62c529b3cc6de549d763f101e706e
Jorge González <yo@jorgeglz.io>
2021-04-26 11:32:32 -0500

    Install PHP Deployer, testing Github Action

---
+++ deploy.php
@@ -0,0 +1,43 @@
+<?php
+namespace Deployer;
+
+use Dotenv\Dotenv;
+
+require 'recipe/laravel.php';
+
+// Load .env
+require_once __DIR__.'/vendor/autoload.php';
+
+Dotenv::createImmutable(__DIR__)->load();
+
+// Config
+
+set('application', 'FIMEats');
+set('deploy_path', '/var/www/fimeats');
+set('repository', 'git@github.com:iksaku/FIMEats.git');
+set('dotenv', '{{current_path}}/.env');
+
+// Hosts
+
+host('fimeats.jorgeglz.io')
+    ->setHostname(env('DEPLOY_HOST'))
+    ->setRemoteUser(env('DEPLOY_USER'));
+
+// Tasks
+
+before('deploy:publish', function () {
+    // Create DB and import menus
+    artisan('import:menus', ['skipIfNoEnv'])();
+
+    // Upload assets built on CI
+    upload(
+        [
+            'public/css',
+            'public/js',
+            'public/mix-manifest.json',
+        ],
+        '{{release_path}}/public'
+    );
+});
+
+after('deploy:failed', 'deploy:unlock');
--- recipe/assets.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Deployer;
-
-desc('Remove Images Folder');
-task('assets:clear:img', function () {
-    run('rm -rf {{release_path}}/public/img/');
-});
