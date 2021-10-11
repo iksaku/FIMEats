fc82fbad7d0acd07b66a7244df3c4a08ab953a54
Jorge González <yo@jorgeglz.io>
2021-10-11 00:17:15 -0500

    Improve scroll behaviour on route navigation

--- src/router.ts
+++ src/router.ts
@@ -4,12 +4,14 @@ import routes from 'virtual:generated-pages'
 export default createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior(to) {
+  scrollBehavior(to, _, savedPosition) {
     if (to.hash) {
       return {
         el: to.hash,
         behavior: 'smooth',
       }
     }
+
+    return savedPosition ?? { top: 0 }
   },
 })
